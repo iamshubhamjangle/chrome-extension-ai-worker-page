bd13f7eb90a0aaf17662d1ebd6fdfe5f4a25602b
Shubham Jangle <jangleshubham13@gmail.com>
2025-01-30 17:28:14 +0530

    refactor: remove devtools page and update popup layout with new features

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from "react";
+import logo from "@assets/img/logo.svg";
 
 export default function Popup() {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-[35rem] p-5 rounded-[10px]">
+      <h2 className="text-xl font-bold text-center">
+        Screenshots to Automate Code
+      </h2>
+      <p id="status"></p>
+      <div className="relative">
+        <button
+          id="clear-screenshot-button"
+          className="absolute cursor-pointer bg-white font-semibold text-[black] z-[1] m-[5px] px-2.5 py-[5px] rounded-lg border-[none] right-0 top-0"
         >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+          Clear
+        </button>
+        <div id="screenshot-container"></div>
+      </div>
+      <label htmlFor="prompt-textarea" className="text-orange-500">
+        Write a prompt
+      </label>
+      <textarea
+        id="prompt-textarea"
+        className="w-full h-[100px] border box-border mx-0 my-[5px] p-2.5 rounded-lg border-solid border-orange-500 focus:border-orange-600 focus:outline-none"
+        placeholder=""
+      ></textarea>
+      <div>
+        <label htmlFor="framework-select" className="text-orange-500">
+          Choose framework:
+        </label>
+        <select
+          id="framework-select"
+          className="w-full border bg-[white] text-sm mx-0 my-[5px] p-2.5 rounded-lg border-solid border-orange-500 focus:border-orange-600 focus:outline-none"
+        >
+          <option value="selenium">Selenium</option>
+          <option value="cypress">Cypress</option>
+        </select>
+      </div>
+      <button
+        id="submit-button"
+        className="bg-orange-500 rounded-full px-6 py-2 text-white text-xs font-bold mx-auto block cursor-pointer"
+      >
+        Run it
+      </button>
+      <div className="relative">
+        <button className="absolute cursor-pointer bg-orange-100 font-semibold text-[black] z-[1] m-[5px] px-2.5 py-[5px] rounded-lg border-[none] right-0 top-0">
+          Copy
+        </button>
+        <div id="openai-response"></div>
+      </div>
     </div>
   );
 }
